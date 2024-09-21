4c6eca270e4b5197668675162f475b6045599619
Alex Straton <alexandrastraton1@gmail.com>
2024-09-21 18:52:21 +0100

    added and styled the footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Logo from "./components/Logo";
 import Form from "./components/Form";
-
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
 export default function App() {
   return (
     <>
       <Logo />
       <Form />
+      <PackingList />
+      <Stats />
     </>
   );
 }
---
+++ src/components/PackingList.jsx
@@ -0,0 +1,12 @@
+export default function PackingList() {
+  return (
+    <div className='list'>
+      <select>
+        <option value='1'>Sort by</option>
+        <option value='2'>Sort by</option>
+        <option value='3'>Sort by</option>
+      </select>
+      <button>Clear List</button>
+    </div>
+  );
+}
