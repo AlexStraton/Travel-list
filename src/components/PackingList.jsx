f421cab94435ef4057bab7f52ff7e733b0d5e80b
Alex Straton <alexandrastraton1@gmail.com>
2024-09-21 19:26:29 +0100

    added a list of default items in the packingList component with a checkbox and delete button

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,6 +1,27 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function PackingList() {
   return (
     <div className='list'>
+      {initialItems.map((item) => {
+        return (
+          <ul>
+            <li key={item.id}>
+              <input type='checkbox' />
+              <span
+                style={item.packed ? { textDecoration: "line-through" } : {}}>
+                {item.quantity}
+                {item.description}
+              </span>
+              <button>❌</button>
+            </li>
+          </ul>
+        );
+      })}
+
       <select>
         <option value='1'>Sort by</option>
         <option value='2'>Sort by</option>
