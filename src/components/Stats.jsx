7bf448f2aec9c517fb865e01a32b78588f06fdbb
Alex Straton <alexandrastraton1@gmail.com>
2024-09-24 11:36:12 +0100

    conditionally rended Stats messages depending on number of items packed

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,13 +3,16 @@ export default function Stats({ items }) {
   const packed = items.filter((item) => item.packed).length;
   const percent = (packed / itemsArray) * 100;
 
-  console.log(packed);
   return (
     <footer className='stats'>
-      <em>
-        🛄You have {itemsArray} items on your list, and you already packed{" "}
-        {packed} ({Math.round(percent)}%)
-      </em>
+      {percent === 100 ? (
+        <em>You got everything! It's time to go ✈️</em>
+      ) : (
+        <em>
+          🛄You have {itemsArray} items on your list, and you already packed{" "}
+          {packed} ({Math.round(percent)}%)
+        </em>
+      )}
     </footer>
   );
 }
